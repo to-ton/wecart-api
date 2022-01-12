1fffc0b96812db9d7d3adf62a5d7a46d0dfea241
ton <32086016+to-ton@users.noreply.github.com>
2022-01-12 08:22:26 +0800

    Update conn.php

--- wecart-api/conn.php
+++ wecart-api/conn.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "id17820533_wecart2021";
-$password = "EYB#Cb?Q[)88(>U%";
-$db = "id17820533_wecart";
+$servername = "";
+$username = "";
+$password = "";
+$db = "";
 
 // Create connection
 $mysqli = mysqli_connect($servername, $username, $password, $db);
